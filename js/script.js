47a4a716593ba3aeecc6f9dedacbcddf33fc67b8
crasnencov <crasnencov@gmail.com>
2016-12-02 11:14:00 +0200

    Added cleaning a text input on click of a submit button

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@ var app = angular.module("root", []);
 app.controller("myCtrl", function ($scope) {
         $scope.users = [];
         $scope.addUser = function () {
-            //$scope.json = angular.toJson($scope.user);
+            var date = new Date();
+            $scope.user.date = date.getTime();
             $scope.users.push({
+                id: $scope.user.date,
                 title: $scope.user.title,
                 body: $scope.user.body
             });
+
             console.log($scope);
         }
+        $scope.clearInput = function () {
+            $scope.user.title = null;
+            $scope.user.body = null;
+        }
+
     }
 );
